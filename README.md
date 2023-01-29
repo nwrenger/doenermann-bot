912c70697f421f35b5b8e53cb5cfc32b8ccbb96a
NWrenger <nils@wrenger.net>
2023-01-29 17:46:57 +0100

    :sparkles: Added a Bunch of New features

--- README.md
+++ README.md
@@ -5,32 +5,33 @@ Now it can:
 - 
 - Copy Message of a Channel in a file
 - Show with a command how many were copied(/count)
-- a normal ping command(/ping)
-- a /stop command which will stop the bot, only the owner(discord-id) of the bot can do that
+- a normal ping(/döner) command
+- a /stop command which will stop the bot, only the owner(discord-id) of the bot can dot that
+- Add your birthday with /set_birthday
+- show the next upcomming birthdays(/next_birthdays)
+- Give a user a Birhtday Role when the user has Birthday
+- Give a user a Member Role when the user joins the server
 
 Dependencies:
 -
 - Python 3.11.1
 - discord.py
 - datetime(module)
+- typing(module)
+- pathlib(module)
+- csv(module)
 
 Usage:
 -
 - First you have to add an Application in the Discord Developer Portal(https://discord.com/developers/applications) and create a bot 
 - After that you paste your bot token in token.env and have to enable all of the of the Privileged Gateway Intents options in the options of your bot
-- Now you have to add your server id, your id and the channel id(where you like the files to be copied from) to bot.py:
+- Now you have to add your server id, your id, the channel id(where you like the files to be copied from), the id of the Birthday Role and the id of the Member role(you have to create those) to bot.py:
 ```python
 server = discord.Object(id=YOUR_SERVER_ID)
 owner = YOUR_ID
 copied_channel = CHANNEL_ID
-```
-- Lastly you have to add the Path for the token.env file to bot.py:
-```python
-token = open("THE_PATH_OF_token.env_FILE", "r").read()
-```
-- And the Path for the citations.txt file to bot.py:
-```python
-file = open("THE_PATH_OF_citations.txt_FILE", "a")
+birthday_role = BIRTHDAY_ROLE_ID
+member_role = MEMBER_ROLE_ID
 ```
 - You can start the bot by running bot.py:
 ```shell
--- bot.py
+++ bot.py
@@ -1,29 +1,56 @@
-import datetime
+from datetime import datetime, date
+from discord.ext import tasks
+from typing import Dict, List, Tuple
 import discord
 from discord import app_commands
+from pathlib import Path
+
+import csv
+
+bot_dir = Path(__file__).parent
+token_file = bot_dir / "token.env"
+birthdays_file = bot_dir / "birthdays.csv"
+citations_file = bot_dir / "citations.txt"
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-token = open("THE_PATH_OF_token.env_FILE", "r").read()
+token = token_file.read_text()
 counts = 0
-server = discord.Object(id=YOUR_SERVER)
+server = discord.Object(id=YOUR_SERVER_ID)
 owner = YOUR_ID
 copied_channel = CHANNEL_ID
+birthday_role = BIRTHDAY_ROLE_ID
+member_role = MEMBER_ROLE_ID
+DATE_FMT = "%d.%m.%Y"
+
+birthdays: Dict[int, date] = {}
+
+if birthdays_file.exists():
+    with birthdays_file.open() as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            birth = datetime.strptime(row["birthday"], DATE_FMT).date()
+            birthdays[int(row["user"])] = birth
+
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
+
     async def setup_hook(self):
         self.tree.copy_global_to(guild=server)
         await self.tree.sync(guild=server)
 
+
 bot = MyClient(intents=intents)
 
-file = open("THE_PATH_OF_citations.txt_FILE", "a")
-file.write("Begin of Copying on " + str(datetime.datetime.now()) + ":\n")
-file.flush()
+
+citations = citations_file.open("a")
+citations.write("%Begin of Copying on " + str(datetime.now()) + ":\n")
+citations.flush()
+
 
 @bot.event
 async def on_ready():
@@ -31,23 +58,81 @@ async def on_ready():
 
 
 @bot.tree.command()
-async def ping(interaction: discord.Interaction):
-    """Ping?"""
-    await interaction.response.send_message('Pong')
+async def döner(interaction: discord.Interaction):
+    """Döner bestellen?"""
+    await interaction.response.send_message("Ne diggi, denkste ich habe das Geld dafür? Aber hier das sollte dir helfen: https://www.lieferando.de/lieferservice/doener/hannover-30159")
+
 
 @bot.tree.command()
 async def stop(interaction: discord.Interaction):
     """Shut the Bot down!"""
     if interaction.user.id == owner:
-        await interaction.response.send_message('Shutting Down')
+        e = discord.Embed(title="Shutting Down!",
+                          description="", color=0x00ff15)
+        await interaction.response.send_message(embed=e)
         exit()
     else:
-        await interaction.response.send_message('You Dont have the permission to do that!')
+        e = discord.Embed(
+            title="You Dont have the permission to do that!", description="", color=0xff0000)
+        await interaction.response.send_message(embed=e)
+
 
 @bot.tree.command()
 async def count(interaction: discord.Interaction):
     """Gives the Count of the already Recorded Messages after last Start"""
-    await interaction.response.send_message(counts)
+    e = discord.Embed(title="Already recorded messages: " +
+                      str(counts), description="", color=0x00ff15)
+    await interaction.response.send_message(embed=e)
+
+
+@bot.tree.command()
+async def next_birthdays(interaction: discord.Interaction):
+    """The next 10 Upcomming Birthdays"""
+    now = datetime.now().date()
+    births_tmp: List[Tuple[int, date, date]] = []
+    for user, birth in birthdays.items():
+        next = calc_birthday(birth)
+        births_tmp.append([user, birth, next])
+
+    births_tmp.sort(key=lambda r: r[2])
+
+    e = discord.Embed(title="Next Birthdays:", description="", color=0x0800ff)
+    for user, birth, next in births_tmp[:10]:
+        age = now.year - birth.year
+        if [now.month, now.day] < [birth.month, birth.day]:
+            age -= 1
+        e.add_field(name=next.strftime("%B %d %Y"),
+                    value=f"<@{user}> ({age})", inline=False)
+    await interaction.response.send_message(embed=e)
+
+
+@bot.tree.command()
+@app_commands.describe(
+    birth="Your Birthday"
+)
+async def set_birthday(interaction: discord.Interaction, birth: str):
+    """Set your Birhtday"""
+    user = interaction.user.id
+    try:
+        birthdays[user] = datetime.strptime(birth, DATE_FMT).date()
+    except Exception:
+        e = discord.Embed(title="Invalid date!",
+                          description="", color=0xff0000)
+        await interaction.response.send_message(embed=e)
+        return
+
+    with birthdays_file.open("w+") as csv_file:
+        header = ["birthday", "user"]
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+        writer.writeheader()
+        for user, birth_date in birthdays.items():
+            writer.writerow(
+                {"birthday": birth_date.strftime(DATE_FMT), "user": user})
+
+    e = discord.Embed(title="Your Birthday was set to: " +
+                      birth, description="", color=0x0800ff)
+    await interaction.response.send_message(embed=e)
+
 
 @bot.event
 async def on_message(message):
@@ -59,7 +144,29 @@ async def on_message(message):
         # print(counts)
         user = str(message.author) + ": "
         print(user + line)
-        file.write(user + line + "\n")
-        file.flush()
+        citations.write(user + line + "\n")
+        citations.flush()
+
+
+@tasks.loop(hours=24)
+async def on_day():
+    now = datetime.now().date()
+    for user, birth in birthdays.items():
+        if calc_birthday(birth) == now:
+            await user.add_roles(user.guild.get_role(birthday_role))
+
+
+@bot.event
+async def on_member_join(user):
+    await user.add_roles(user.guild.get_role(member_role))
+
+
+def calc_birthday(birth: date) -> date:
+    now = datetime.now().date()
+    next = birth.replace(year=now.year)
+    if next < now:
+        next = next.replace(next.year + 1)
+    return next
+
 
 bot.run(token)
