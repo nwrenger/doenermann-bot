fb3969361505e32dc723564f20b1ecb30f6983f9
NWrenger <nils@wrenger.net>
2023-04-25 22:05:20 +0200

    :sparkles: Finished Work on next_birthdays rs, now works!

--- src/commands/next_birthdays.rs
+++ src/commands/next_birthdays.rs
@@ -1,11 +1,44 @@
+use csv::Reader;
+use chrono::{NaiveDate, Local, Datelike};
 use serenity::builder::{CreateApplicationCommand, CreateEmbed};
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Row {
+    birthday: String,
+    user: u64,
+}
 
 pub fn run(_options: &[CommandDataOption]) -> (String, CreateEmbed) {
     let mut embed = CreateEmbed::default();
     embed.title("Next Birthdays:");
-    for i in 0..10 {
-        embed.field((i + 1).to_string(), "Test Text", false);
+    let mut rdr = Reader::from_path("birthdays.csv").unwrap();
+    let mut rows: Vec<Row> =
+        rdr.deserialize().map(|result| result.unwrap()).collect();
+    let now = Local::now().date_naive();
+    
+    rows.sort_by_key(|row| {
+        let birthday = NaiveDate::parse_from_str(&row.birthday, "%Y-%m-%d").unwrap();
+        let next_birthday = if birthday.with_year(now.year()) < now.with_year(now.year()) {
+            birthday.with_year(now.year() + 1).unwrap()
+        } else {
+            birthday.with_year(now.year()).unwrap()
+        };
+        next_birthday.signed_duration_since(now).num_days().abs()
+    });
+
+    for i in rows.drain(..10) {
+        let date = NaiveDate::parse_from_str(&i.birthday, "%Y-%m-%d").unwrap();
+        let future = if date.with_year(now.year()) < now.with_year(now.year()) {
+            date.with_year(now.year() + 1).unwrap()
+        } else {
+            date.with_year(now.year()).unwrap()
+        };
+        embed.field(
+            future.format("%d %B %Y").to_string(),
+            format!("<@{}> ({})", i.user, (Local::now().date_naive().years_since(date).unwrap() + 1)),
+            false,
+        );
     }
     ("".to_string(), embed)
 }
--- src/commands/set_birthday.rs
+++ src/commands/set_birthday.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveDate;
+use chrono::{NaiveDate, Local, Datelike};
 use csv::{Reader, Writer};
 use serde;
 use serenity::builder::{CreateApplicationCommand, CreateEmbed};
@@ -19,7 +19,7 @@ pub fn run(options: &[CommandDataOption], user: u64) -> (String, CreateEmbed) {
     match options.get(0) {
         Some(option) => match &option.resolved {
             Some(CommandDataOptionValue::String(value)) => {
-                if NaiveDate::parse_from_str(value, date_fmt).is_ok() {
+                if NaiveDate::parse_from_str(value, date_fmt).is_ok() && Local::now().date_naive().years_since(NaiveDate::parse_from_str(value, date_fmt).unwrap()).is_some() {
                     let mut rdr = Reader::from_path("birthdays.csv").unwrap();
                     let mut rows: Vec<Row> =
                         rdr.deserialize().map(|result| result.unwrap()).collect();
@@ -51,12 +51,16 @@ pub fn run(options: &[CommandDataOption], user: u64) -> (String, CreateEmbed) {
                     embed.title("Your Birthday was set to: ".to_string() + value)
                 } else {
                     embed.title("Invalid Date!");
-                    embed.description(
-                        &NaiveDate::parse_from_str(value, date_fmt)
-                            .err()
-                            .unwrap()
-                            .to_string(),
-                    )
+                    if Local::now().date_naive().years_since(NaiveDate::parse_from_str(value, date_fmt).unwrap()).is_some() {
+                        embed.description(
+                            &NaiveDate::parse_from_str(value, date_fmt)
+                                .err()
+                                .unwrap()
+                                .to_string(),
+                        )
+                    } else {
+                        embed.description(format!("Stop it Erik! You can set Dates over the current Year! ({})", Local::now().date_naive().year()))
+                    }
                 }
             }
             _ => panic!("Expected String type"),
