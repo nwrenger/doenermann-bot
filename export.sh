213911be07d080b6ae1d2ac2117ee854876e9950
nwrenger <nils@wrenger.net>
2023-07-07 00:45:48 +0200

    :wrench: Updated Naming

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 cargo build -r
 cp target/release/doenermann-bot ./
-zip -r exp/doenermann-bot_lin.zip doenermann-bot .env citations.txt birthdays.csv
+zip -r exp/doenermann-bot_v1.0.3.zip doenermann-bot .env citations.txt birthdays.csv
 rm doenermann-bot
